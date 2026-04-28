5678429f89f67a6aafdd9170d8a3dd540b34d85a
Pascua1Quack <e3l10@hotmail.com>
2026-04-28 16:13:37 +0000

    se agrego el componente y la funcion en la pagina principal para mostrar las tarjetas de los semestres, ademas se agrego el componente de las tarjetas de los semestres

--- proyecto_paguina_asesorias/app/page.tsx
+++ proyecto_paguina_asesorias/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
+import { TarjetasSemestre } from "./components/TarjetasSemestre/TarjetasSemestre";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <TarjetasSemestre />
       <Footer />
-      
     </>
   );
 }
