0bc4d8ba4824f1c55124b8aeae0175136a47428e
Pascua1Quack <e3l10@hotmail.com>
2026-04-28 16:46:06 +0000

    modifique el page para que corra lo de las materias

--- proyecto_paguina_asesorias/app/page.tsx
+++ proyecto_paguina_asesorias/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
 import { TarjetasSemestre } from "./components/TarjetasSemestre/TarjetasSemestre";
+import { TarjetasMaterias } from "./components/TarjetasMateria/TarjetasMaterias";
 
 export default function Home() {
   return (
     <>
       <Header />
       <TarjetasSemestre />
+      <TarjetasMaterias />
       <Footer />
     </>
   );
