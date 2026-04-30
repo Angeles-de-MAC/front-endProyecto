f0023365f30ab414a27d4eba8af321897a29a29e
mxnlinesdev <mxnlinesdev@gmail.com>
2026-04-30 03:59:46 +0000

    Se agrega a la pagina principal Recursos recientes y Comparte tu conocimiento.
    Se actualizan paddings.
    
    TEMPORALMENTE, la pagina es un client component.
    
    Se actualiza el import de  TarjetasMaterias.

--- proyecto_paguina_asesorias/app/page.tsx
+++ proyecto_paguina_asesorias/app/page.tsx
@@ -1,14 +1,23 @@
+"use client" // Jamás en la vida hagan esto en un layout.tsx o page.tsx
+
 import Image from "next/image";
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
 import { TarjetasSemestre } from "./components/TarjetasSemestre/TarjetasSemestre";
 import { TarjetasMaterias } from "./components/TarjetasMateria/TarjetasMaterias";
+import RecursosRecientes from "./recursos-recientes/RecursosRecientes";
+import ComparteTuConocimiento from "./comparte-tu-conocimiento/ComparteTuConocimiento";
 
 export default function Home() {
   return (
     <>
       <Header />
       <TarjetasSemestre />
+
+      <RecursosRecientes/>
+
+      <ComparteTuConocimiento/>
+
       <TarjetasMaterias />
       <Footer />
     </>
